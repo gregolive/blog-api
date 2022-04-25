0b0368be74b2362a296818181b8257b3e4c43650
Gregory Olive <gregoryolive@outlook.com>
2022-04-25 11:08:55 +0900

    Set up basic app and add dependencies

---
+++ bin/www.js
@@ -0,0 +1,11 @@
+import app from '../app.js';
+import http from 'http';
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () =>
+  console.log(`App listening on port 3000.`),
+);
